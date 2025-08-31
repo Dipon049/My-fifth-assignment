50f63cb69661a9d31cc59dc01a21d847980d2149
DIPON DATTA <dipondattaict4.com>
2025-08-31 17:36:39 +0600

    added css styles and javascript function

--- script.js
+++ script.js
@@ -10,19 +10,44 @@
     
          const newCoinCount = coinCount - 20 ;
          document.getElementById("coin_count").innerText = newCoinCount ;
+
+         
+        // document.getElementById("call-history").innerText = "National Emergency Number \n 999";
+
+         const still = new Date();
+          const hours = still.getHours();
+           const minutes = still.getMinutes();
+            const seconds = still.getSeconds();
+
+           document.getElementById("call-history").innerText = "National Emergency Number \n Service Number: 999\n" + (`Current Time: ${hours}:${minutes}:${seconds}`);
      }
+
+         
      else{
         alert("You have less than 20 coin");
      }
-   
+        
   
 
  })
  document.getElementById("love_click").addEventListener("click", function(){
 
-    // const loveClick = document.getElementById("love_click").innerText;
       const heartCount = document.getElementById("heart_count").innerText
 
             const newHeartCount = parseInt(heartCount) + 1;
         document.getElementById("heart_count").innerText = parseInt(newHeartCount)  ;
  })
+
+ document.getElementById("copy_click").addEventListener("click", function(){
+
+    alert("Copied the service number (999)");
+
+    const copyCount = document.getElementById("copy_count").innerText;
+    const newCopyCount = parseInt(copyCount) + 1; 
+    document.getElementById("copy_count").innerText = parseInt (newCopyCount);
+
+    navigator.clipboard.writeText(999);
+ })
+
+ 
+ 
